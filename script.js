e90b4c2cac45b6fc06e001c724eda6a5145e8062
NunoFlorido-dev <126115463+NunoFlorido-dev@users.noreply.github.com>
2023-06-02 22:04:03 +0100

    Add dynamically created grid

--- script.js
+++ script.js
@@ -4,13 +4,19 @@ const createGrid = (amtOfGrids) => {
   for(let i = 0; i < amtOfGrids; i++){
     const row = document.createElement('div');
     row.classList.add('grid-row');
+   
     for(let j = 0; j < amtOfGrids; j++){
+      const widthAndHeight = 969 / sizeOfGrid;
       const gridBox = document.createElement('div');
       gridBox.classList.add('grid-box');
+      gridBox.style.width = `${widthAndHeight}px`;
+      gridBox.style.height = `${widthAndHeight}px`;
+
       row.appendChild(gridBox);
     }
+    
     container.appendChild(row);
   }
 }
-let sizeOfGrid = 16;
+let sizeOfGrid = 4;
 createGrid(sizeOfGrid);
