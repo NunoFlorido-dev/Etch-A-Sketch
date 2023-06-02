d83ca56e2b56d53523aedd0a4e1e98ea6f6e0bf5
NunoFlorido-dev <126115463+NunoFlorido-dev@users.noreply.github.com>
2023-06-02 22:50:44 +0100

    Create button and event

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
 const container = document.querySelector('.gridContainer');
+const buttonReset = document.querySelector('.button');
+
 
 const createGrid = (amtOfGrids) => {
-  for(let i = 0; i < amtOfGrids; i++){
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('wrapper');
+
+  for (let i = 0; i < amtOfGrids; i++) {
     const row = document.createElement('div');
     row.classList.add('grid-row');
-   
-    for(let j = 0; j < amtOfGrids; j++){
-      const widthAndHeight = 969 / sizeOfGrid;
+
+    for (let j = 0; j < amtOfGrids; j++) {
+      const widthAndHeight = 960 / amtOfGrids;
       const gridBox = document.createElement('div');
       gridBox.classList.add('grid-box');
       gridBox.style.width = `${widthAndHeight}px`;
       gridBox.style.height = `${widthAndHeight}px`;
+      //adding mouseenter event listener for coloring the grid
       gridBox.addEventListener('mouseenter', () => {
         gridBox.style.backgroundColor = 'black';
       })
       row.appendChild(gridBox);
     }
-    
-    container.appendChild(row);
+
+    wrapper.appendChild(row);
   }
+  container.appendChild(wrapper);
 }
-let sizeOfGrid = 4;
-createGrid(sizeOfGrid);
+
+buttonReset.addEventListener('click', () => {
+  let userSize = Number(prompt('Enter the size of the grid'));
+
+  if (userSize > 100) {
+    userSize = Number(prompt('Pick a number less than 100'));
+  }
+  const wrapper = document.querySelector('.wrapper');
+
+  if (!wrapper) {
+    createGrid(userSize);
+  } else {
+    wrapper.remove();
+    createGrid(userSize)
+  }
+
+})
+
